d21cb088e2fa9d7f7b16d94dde4c8ee10c881545
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2024-03-31 23:21:22 +0200

    update Expert Book && Expert Roadmap models

--- model/expertBookReviews.py
+++ model/expertBookReviews.py
@@ -20,3 +20,14 @@ def short_description(self):
             'book_id': self.book_id,
             'rating': self.rating
         }
+        
+def full_description(self):
+
+        return {
+            'review_id': self.review_id,
+            'expert_id': self.expert_id,
+            'book_id': self.book_id,
+            'rating': self.rating,
+            'book_review': self.book_review,
+            'book_summary': self.book_summary
+        }
--- model/expertRoadmapReviews.py
+++ model/expertRoadmapReviews.py
@@ -8,3 +8,22 @@ class ExpertRoadmapReviews(db.Model):
     expert_id = db.Column(db.Integer, db.ForeignKey('expert.expert_id'))
     rating = db.Column(db.Integer)
     roadmap_review = db.Column(db.Text)
+    
+    def short_description(self):
+
+        return {
+            'review_id': self.review_id,
+            'roadmap_id': self.roadmap_id,
+            'rating': self.rating
+        }
+        
+def full_description(self):
+
+        return {
+            'review_id': self.review_id,
+            'expert_id': self.expert_id,
+            'roadmap_id': self.roadmap_id,
+            'rating': self.rating,
+            'roadmap_review': self.roadmap_review
+        }
+        
