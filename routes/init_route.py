1ec4eb3893c59990f8351ed3dffce8c353c36f12
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-04-04 13:44:44 +0200

    Test Learner Book & Roadmap reviews routes (Done)

--- routes/init_route.py
+++ routes/init_route.py
@@ -4,9 +4,13 @@ from database import db
 from routes.book_route import book_routes
 from routes.expert_book_reviews_route import expert_book_reviews_route
 from routes.expert_roadmap_reviews_route import expert_roadmap_reviews_route
+from routes.learner_book_review_route import learner_book_reviews_route
+from routes.learner_roadmap_review_route import learner_roadmap_reviews_route
 
 
 def init_routes(app):
     book_routes(app, db)
     expert_book_reviews_route(app, db)
     expert_roadmap_reviews_route(app, db)
+    learner_book_reviews_route(app, db)
+    learner_roadmap_reviews_route(app, db)
